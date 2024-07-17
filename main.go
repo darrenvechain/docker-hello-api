e8e7e70ad6289716a0014c3803b2fefc4e0547fa
Darren Kelly <darren.kelly@vechain.org>
2024-07-17 09:40:20 +0100

    feat: add forwarding URL

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,10 +15,29 @@ func main() {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	message := "Hello, World!"
-	envVar, ok := os.LookupEnv("TEST_ENV_VAR")
+	forwardingUrl, ok := os.LookupEnv("FORWARDING_URL")
+	fmt.Println("Forwarding URL:", forwardingUrl)
 	if ok {
-		message = fmt.Sprintf("Hello, %s!", envVar)
+		res, err := http.Get(forwardingUrl)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %v", err)
+			return
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Fprintf(w, "Error reading body: %v", err)
+			return
+		}
+
+		w.Write(body)
+	} else {
+		message := "Hello, World!"
+		envVar, ok := os.LookupEnv("TEST_ENV_VAR")
+		if ok {
+			message = fmt.Sprintf("Hello, %s!", envVar)
+		}
+		fmt.Fprint(w, message)
 	}
-	fmt.Fprint(w, message)
 }
